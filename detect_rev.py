8d0bf3e0890fa730e222a9c5609eea85a6fe0a70
ricardotran92 <quanghung.850621@gmail.com>
2024-01-10 15:31:34 +0700

    update detect_rev.py

--- detect_rev.py
+++ detect_rev.py
@@ -150,6 +150,10 @@ def run(
     # Run inference
     model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
     seen, windows, dt = 0, [], (Profile(device=device), Profile(device=device), Profile(device=device))
+    
+    # Create a DataFrame to store the data
+    df = pd.DataFrame(columns=["Time", "Vehicle Object", "Direction"]) # new3
+
     for path, im, im0s, vid_cap, s in dataset:
         with dt[0]:
             im = torch.from_numpy(im).to(model.device)
@@ -195,9 +199,6 @@ def run(
         rects = [] # new
         # Step 1: Initialize a dictionary to store bounding boxes of objects # new2
         bounding_boxes = {} # new2
-
-        # Create a DataFrame to store the data
-        df = pd.DataFrame(columns=["Time", "Vehicle Object", "Direction"]) # new3
 
         # Process predictions
         for i, det in enumerate(pred):  # per image
@@ -363,10 +364,13 @@ def run(
                         save_path = str(Path(save_path).with_suffix(".mp4"))  # force *.mp4 suffix on results videos
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
                     vid_writer[i].write(im0)
-
+   
         # Print time (inference-only)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
 
+    # At the end of the video, write the DataFrame to a CSV file
+    df.to_csv('vehicle_analysis.csv', index=False) # new3
+    
     # Print results
     t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
     LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
@@ -375,9 +379,6 @@ def run(
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
-
-    # At the end of the video, write the DataFrame to a CSV file
-    df.to_csv('vehicle_analysis.csv', index=False) # new3
 
 def parse_opt():
     parser = argparse.ArgumentParser()
